d2acddbfd5909a51d71eb6c4257a716c43c9f6e7
disrupted <salomon.popp@gmail.com>
2017-05-16 17:10:32 +0200

    init

---
+++ src/postfix/Postfix.java
@@ -0,0 +1,19 @@
+package postfix;
+
+public class Postfix {
+
+  public static void main(String[] args) {
+
+  }
+
+  public String infixToPostfix(String infix) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public double evaluate(String postfix) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+}
---
+++ src/stack/LinkedListStack.java
@@ -0,0 +1,29 @@
+package stack;
+
+public class LinkedListStack<E> implements Stack<E> {
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E top() throws Underflow {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Stack<E> push(E element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Stack<E> pop() throws Underflow {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
