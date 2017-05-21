986879fcd19443fc39952f3a12e5a97637292aff
disrupted <salomon.popp@gmail.com>
2017-05-21 18:19:37 +0200

    working postfix evalution using Stack methods

--- src/postfix/Postfix.java
+++ src/postfix/Postfix.java
@@ -2,11 +2,10 @@ package postfix;
 
 import stack.LinkedListStack;
 import stack.Stack;
-
-import java.util.ArrayList;
+import stack.Underflow;
 
 public class Postfix {
-  Stack<String> stack;
+  Stack<Double> stack;
 
   public Postfix() {
     stack = new LinkedListStack<>();
@@ -18,58 +17,6 @@ public class Postfix {
     pf.evaluate("Test1+2");
   }
 
-  public String infixToPostfix(String infix) {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  public double evaluate(String postfix) {
-    // TODO Auto-generated method stub
-    ArrayList<Integer> arr = new ArrayList<>();
-    double result = 0;
-    for (int i = 0; i < postfix.length(); i++) {
-      String c = String.valueOf(postfix.charAt(i));
-      if (isInt(c)) {
-        arr.add(Integer.parseInt(c));
-      } else {
-        switch (c) { // find operator
-          case "+":
-            System.out.println("Addition");
-            for (int e : arr
-                    ) {
-              result += e;
-            }
-            break;
-          case "-":
-            System.out.println("Subtraction");
-            result = arr.get(0);
-            for (int j = 1; j < arr.size(); j++) {
-              result -= arr.get(j);
-            }
-            break;
-          case "*":
-            System.out.println("Multiplication");
-            result = 1;
-            for (int e : arr
-                    ) {
-              result *= e;
-            }
-            break;
-          case "/":
-            System.out.println("Division");
-            result = arr.get(0);
-            for (int j = 1; j < arr.size(); j++) {
-              result /= arr.get(j);
-            }
-            break;
-          default:
-            break;
-        }
-      }
-    }
-    return result;
-  }
-
   public static boolean isInt(String s) {
     try {
       Integer.parseInt(s);
@@ -77,6 +24,55 @@ public class Postfix {
       return false;
     }
     return true;
+  }
+
+  public String infixToPostfix(String infix) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public double evaluate(String postfix) {
+    // TODO Auto-generated method stub
+    double val1 = 0;
+    double val2 = 0;
+    String[] operands = postfix.split(" ");
+    for (String operand : operands) {
+      if (isInt(operand)) {
+        stack.push(Double.parseDouble(operand));
+      } else {
+        try {
+          val1 = stack.pop();
+          val2 = stack.pop();
+        } catch (Underflow ignored) {
+        }
+        switch (operand) { // find operator
+          case "+":
+            System.out.println("Addition");
+            stack.push(val2 + val1);
+            break;
+          case "-":
+            System.out.println("Subtraction");
+            stack.push(val2 - val1);
+            break;
+          case "*":
+            System.out.println("Multiplication");
+            stack.push(val2 * val1);
+            break;
+          case "/":
+            System.out.println("Division");
+            stack.push(val2 / val1);
+            break;
+          default:
+            break;
+        }
+      }
+    }
+    try {
+      return stack.pop();
+    } catch (Underflow underflow) {
+      underflow.printStackTrace();
+    }
+    return 0;
   }
 
 }
