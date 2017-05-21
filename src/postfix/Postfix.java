883f5fedc2b9e6f4b633bda6938b771db0622b56
disrupted <salomon.popp@gmail.com>
2017-05-21 17:41:12 +0200

    basic postfix evaluation

--- src/postfix/Postfix.java
+++ src/postfix/Postfix.java
@@ -1,9 +1,12 @@
 package postfix;
 
 import stack.LinkedListStack;
+import stack.Stack;
+
+import java.util.ArrayList;
 
 public class Postfix {
-  LinkedListStack<String> stack;
+  Stack<String> stack;
 
   public Postfix() {
     stack = new LinkedListStack<>();
@@ -12,7 +15,7 @@ public class Postfix {
   public static void main(String[] args) {
     System.out.println("Hello World");
     Postfix pf = new Postfix();
-    pf.evaluate("Test1");
+    pf.evaluate("Test1+2");
   }
 
   public String infixToPostfix(String infix) {
@@ -22,14 +25,49 @@ public class Postfix {
 
   public double evaluate(String postfix) {
     // TODO Auto-generated method stub
+    ArrayList<Integer> arr = new ArrayList<>();
+    double result = 0;
     for (int i = 0; i < postfix.length(); i++) {
       String c = String.valueOf(postfix.charAt(i));
-      System.out.println(c);
       if (isInt(c)) {
-        stack.push(c);
+        arr.add(Integer.parseInt(c));
+      } else {
+        switch (c) { // find operator
+          case "+":
+            System.out.println("Addition");
+            for (int e : arr
+                    ) {
+              result += e;
+            }
+            break;
+          case "-":
+            System.out.println("Subtraction");
+            result = arr.get(0);
+            for (int j = 1; j < arr.size(); j++) {
+              result -= arr.get(j);
+            }
+            break;
+          case "*":
+            System.out.println("Multiplication");
+            result = 1;
+            for (int e : arr
+                    ) {
+              result *= e;
+            }
+            break;
+          case "/":
+            System.out.println("Division");
+            result = arr.get(0);
+            for (int j = 1; j < arr.size(); j++) {
+              result /= arr.get(j);
+            }
+            break;
+          default:
+            break;
+        }
       }
     }
-    return 0;
+    return result;
   }
 
   public static boolean isInt(String s) {
@@ -38,7 +76,6 @@ public class Postfix {
     } catch (NumberFormatException | NullPointerException e) {
       return false;
     }
-    // only got here if we didn't return false
     return true;
   }
 
